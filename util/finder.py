5d0354d14a3847fbc82dcadcba2a179b4a0e99ce
sch0nik <sch-nikolaj@yandex.ru>
2023-05-09 14:11:56 +0300

    переделал логику поиска слов

--- util/finder.py
+++ util/finder.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 def is_matches(template, list_chars, word):
     for i, j in zip(template, word):
         if i == '*':
@@ -20,45 +17,27 @@ def is_matches(template, list_chars, word):
 
 
 def find_word(word, list_chars, not_char, words=None):
+    if not words:
+        return []
     result = []
     bad_letters = set(not_char)
     existing_letters = set(word) | set(''.join(list_chars))
     existing_letters.discard('*')
-    common_letters = existing_letters & bad_letters
+    common_bad_letters = existing_letters & bad_letters
+    common_letters = bad_letters - common_bad_letters
 
     for item in words:
         if len(item) != len(word):
             continue
 
-        # trio = bad_letters & set(word) & set(''.join(list_chars))
-        # if trio:
-        #     counter = [let for let in trio if item.count(let) >= 3]
-
-        if (bad_letters - common_letters) & set(item):
+        if [i for i in common_bad_letters if item.count(i) > 1]:
             continue
-        # if common_letters:
-        #     counter = [item for char in common_letters if item.count(char) >= 2]
-        #     if counter:
-        #         continue
-        # else:
-        #     if bad_letters & set(item):
-        #         continue
+
+        if common_letters & set(item):
+            continue
 
         if not is_matches(word, list_chars, item):
             continue
 
         result.append(item)
     return result
-
-
-if __name__ == '__main__':
-    # шаблон слова
-    sample = '*****'
-    # список букв не на местах
-    list_sample = [
-        '*****',
-    ]
-    # несуществующие буквы
-    not_chars = ''
-
-    pprint(find_word(sample, list_sample, not_chars, ['список слов']))
