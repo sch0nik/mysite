c9fae66def782cda4033112a13bf58b825f22676
sch0nik <sch-nikolaj@yandex.ru>
2023-05-14 18:45:58 +0300

    поменял алгоритм работы

--- util/finder.py
+++ util/finder.py
@@ -1,43 +1,53 @@
-def is_matches(template, list_chars, word):
-    for i, j in zip(template, word):
-        if i == '*':
-            continue
-        if i != j:
+def is_matches(word, template, chars):
+    for ind, ch in enumerate(word):
+        if template[ind] != '*' and ch != template[ind]:
             return False
-    chars = []
-    for item in list_chars:
-        for i, j in enumerate(item):
-            if j == '*':
-                continue
-            chars.append({'index': i, 'item': j})
-    for i in chars:
-        if i['item'] not in word or i['item'] == word[i['index']]:
+        if ch in chars[ind]:
             return False
     return True
 
 
-def find_word(word, list_chars, not_char, words=None):
+def find_word(template, list_chars, not_char, words=None):
     if not words:
         return []
+    length = len(template)
+    set_template = set(template)
+    list_chars = list(map(lambda x: x.ljust(length, '*') if len(x) < length else x[:5], list_chars))
+    bad_chars = set(not_char)
+    existing_chars = set(''.join(list_chars))
+    common_chars = bad_chars & existing_chars & set_template
+    existing_chars = existing_chars | set_template
+    existing_chars.discard('*')
+    common_existing_chars = bad_chars & existing_chars or bad_chars & set_template
+    bad_chars = bad_chars - existing_chars
+
+    chars = [(), (), (), (), ()]
+    for i in list_chars:
+        for ind, j in enumerate(i):
+            chars[ind] += (j,)
+
     result = []
-    bad_letters = set(not_char)
-    existing_letters = set(word) | set(''.join(list_chars))
-    existing_letters.discard('*')
-    common_bad_letters = existing_letters & bad_letters
-    common_letters = bad_letters - common_bad_letters
-
-    for item in words:
-        if len(item) != len(word):
+    for word in words:
+        set_word = set(word)
+        if len(word) != length:
             continue
-
-        if [i for i in common_bad_letters if item.count(i) > 1]:
+        if not existing_chars & set_word:
             continue
-
-        if common_letters & set(item):
+        if set_word & bad_chars:
             continue
-
-        if not is_matches(word, list_chars, item):
+        if common_chars and is_matches(word, template, chars):
+            result.append(word)
             continue
-
-        result.append(item)
+        if common_existing_chars:
+            if [i for i in common_existing_chars if word.count(i) > 1]:
+                continue
+        if not is_matches(word, template, chars):
+            continue
+        result.append(word)
     return result
+
+# with open('../static/text/russian.txt', 'r') as f:
+#     list_word = f.readlines()
+# WORDS = [item[:-1] for item in list_word]
+#
+# print(find_word('а****', ['*****'], 'а', WORDS))
