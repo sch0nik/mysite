c3cbb4ce7cc39b697ef017308d2fccc6f8ca9904
sch0nik <sch-nikolaj@yandex.ru>
2023-05-14 18:45:44 +0300

    исправил тесты

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -18,7 +18,7 @@ def test_letter_presence():
         assert 'а' in word and word[0] != 'а'
     result = find_word('а****', ['*****'], '', WORDS)
     for word in result:
-        assert word[0] == 'а'
+        assert word[0] == 'а', word
 
 
 def test_non_existence():
@@ -29,7 +29,7 @@ def test_non_existence():
         assert 'т' not in word
 
 
-def test_double_letters():
+def test_double_letters_1():
     template = '*****'
     list_template_exist = ['****а']
     bad_letter = 'а'
@@ -46,10 +46,32 @@ def test_double_letters():
         if item.count(bad_letter) > 1:
             continue
         expected.append(item)
-    assert received == expected
+    assert received == expected, set(received) - set(expected)
 
+
+def test_double_letters_2():
     template = 'а****'
     list_template_exist = ['*****']
+    bad_letter = 'а'
+
+    received = find_word(template, list_template_exist, bad_letter, WORDS)
+    expected = []
+    for item in WORDS:
+        if len(item) != len(template):
+            continue
+        if item.count(bad_letter) > 1:
+            continue
+        if item[0] != bad_letter:
+            continue
+        expected.append(item)
+    assert set(received) == set(expected), set(received) - set(expected)
+
+
+def test_double_letters_3():
+    template = 'а****'
+    list_template_exist = ['**а**']
+    bad_letter = 'а'
+
     received = find_word(template, list_template_exist, bad_letter, WORDS)
     expected = []
     for item in WORDS:
@@ -59,22 +81,20 @@ def test_double_letters():
             continue
         if item[0] != bad_letter:
             continue
-        if item.count(bad_letter) > 1:
+        if item[2] == bad_letter:
             continue
         expected.append(item)
-    assert received == expected
+    assert set(received) == set(expected), set(received) - set(expected)
 
-    list_template_exist = ['**а**']
-    received = find_word(template, list_template_exist, bad_letter, WORDS)
-    expected = []
-    for item in WORDS:
-        if len(item) != len(template):
-            continue
-        if bad_letter not in item:
-            continue
-        if item[2] == bad_letter or item[0] != bad_letter:
-            continue
-        if item.count(bad_letter) > 1:
-            continue
-        expected.append(item)
-    assert received == expected
+
+def test_special_case():
+    result = find_word('****а', ['а****'], 'волнбак', WORDS)
+    assert 'жажда' in result
+
+
+def list_difference(list1, list2):
+    return list(set(list1).symmetric_difference(set(list2)))
+
+
+# def list_difference_ab(list1, list2):
+#     return list(set(list1) (set(list2)))
